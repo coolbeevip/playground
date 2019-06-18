03e4216d53242e91506ecc0344ec8304e4b0a46e
Lei Zhang <zhanglei@apache.org>
2019-06-18 10:32:52 +0800

    增加测试用例

--- state_machine_demo/saga-akkafsm/src/test/java/coolbeevip/playgroud/statemachine/saga/SagaApplicationTest.java
+++ state_machine_demo/saga-akkafsm/src/test/java/coolbeevip/playgroud/statemachine/saga/SagaApplicationTest.java
@@ -1,8 +1,6 @@
 package coolbeevip.playgroud.statemachine.saga;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.testkit.javadsl.TestKit;
 import com.google.common.eventbus.EventBus;
 import coolbeevip.playgroud.statemachine.saga.actors.event.SagaEndedEvent;
 import coolbeevip.playgroud.statemachine.saga.actors.event.SagaStartedEvent;
@@ -59,17 +57,5 @@ public class SagaApplicationTest {
     sagaEvents.add(SagaEndedEvent.builder().globalTxId(globalTxId).build());
     return sagaEvents;
   }
-//
-//  private List<SagaEvent> getTxAbortedEventScenarioSagaEvents(String globalTxId){
-//    final String localTxId_1 = UUID.randomUUID().toString();
-//    final String localTxId_2 = UUID.randomUUID().toString();
-//    List<SagaEvent> sagaEvents = new ArrayList<>();
-//    sagaEvents.add(SagaStartedEvent.builder().globalTxId(globalTxId).build());
-//    sagaEvents.add(TxStartedEvent.builder().globalTxId(globalTxId).parentTxId(globalTxId).localTxId(localTxId_1).build());
-//    sagaEvents.add(TxEndedEvent.builder().globalTxId(globalTxId).parentTxId(globalTxId).localTxId(localTxId_1).build());
-//    sagaEvents.add(TxStartedEvent.builder().globalTxId(globalTxId).parentTxId(globalTxId).localTxId(localTxId_2).build());
-//    sagaEvents.add(TxAbortedEvent.builder().globalTxId(globalTxId).parentTxId(globalTxId).localTxId(localTxId_2).build());
-//    sagaEvents.add(TxComponsitedEvent.builder().globalTxId(globalTxId).build());
-//    return sagaEvents;
-//  }
+
 }
